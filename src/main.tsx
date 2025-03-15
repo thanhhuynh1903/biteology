38e60e8866f37111f994d124570e04209361a4cf
thanhhuynh1903 <thanhgaming1234@gmail.com>
2025-03-15 21:28:29 +0700

    fix scroll to top

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,12 @@ import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from "react-router-dom";
 import { ConfigProvider } from 'antd';
+import ScrollToTop from './util/ScrollToTop.tsx';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
+    <ScrollToTop/>
     <ConfigProvider theme={{ token: { colorPrimary: "#4CAF50" } }}>
     <App />
     </ConfigProvider>
